75785e05a069d5792035f1ecf9147cdcfbd77612
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-06-27 10:30:54 +0300

    update

--- database/migrations/2024_06_27_073032_update_events_table.php
+++ database/migrations/2024_06_27_073032_update_events_table.php
@@ -6,38 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::table('events', function (Blueprint $table) {
-            // Add new columns
-            $table->dateTime('start_time')->nullable();
-            $table->dateTime('end_time')->nullable();
-        });
-
-        // Copy data from old columns to new columns
-        DB::statement('UPDATE events SET start_time = date_start, end_time = date_finish');
-
-        // Drop old columns
-        Schema::table('events', function (Blueprint $table) {
             $table->dropColumn('date_start');
             $table->dropColumn('date_finish');
+        });
+
+        Schema::table('events', function (Blueprint $table) {
+            $table->dateTime('start_time')->nullable();
+            $table->dateTime('end_time')->nullable();
         });
     }
 
     public function down()
     {
-        // In case of rollback, reverse the process
+        // Откатываем изменения при необходимости
         Schema::table('events', function (Blueprint $table) {
             $table->dateTime('date_start')->nullable();
             $table->dateTime('date_finish')->nullable();
-        });
-
-        DB::statement('UPDATE events SET date_start = start_time, date_finish = end_time');
-
-        Schema::table('events', function (Blueprint $table) {
             $table->dropColumn('start_time');
             $table->dropColumn('end_time');
         });
