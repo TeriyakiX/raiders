6812720d75ac512e905ca44e749cf81fc13180c5
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-08-24 09:26:22 +0300

    test host 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,17 +18,16 @@ class UserController extends Controller
 
     public function getUserData(Request $request)
     {
-        // Получаем access_tцoken из куки
         $accessToken = $request->cookie('access_token');
+
+        Log::info('Access Token:', ['token' => $accessToken]); // Логирование токена
 
         if (!$accessToken) {
             return response()->json(['message' => 'токена нема'], 401);
         }
 
-        // Вызываем метод getUserData из UserService для выполнения запроса к API
         $userData = $this->userService->getUserData($accessToken);
 
-        // Проверяем наличие ошибок и возвращаем результат
         if (isset($userData['error'])) {
             return response()->json(['message' => $userData['error']], 500);
         } else {
