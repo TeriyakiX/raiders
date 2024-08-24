80de6c08902da692568a62af4b340eded383b9a7
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-08-24 09:36:45 +0300

    test host 3

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,8 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'https://raiders-dev.netlify.app',
-        ],
+        'http://raiedersapi.ru',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
