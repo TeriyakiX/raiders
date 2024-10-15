5ea08f41d7e10884481ef5254091e46196bf09e3
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-10-15 10:35:38 +0300

    corss

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,15 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Routing\Controllers\Middleware;
 
-class CorsMiddleware extends Middleware
-
+class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
         $response = $next($request);
 
+        // Установка заголовков CORS
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
