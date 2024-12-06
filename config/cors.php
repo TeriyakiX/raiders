51f4849e2e305fd143ec086de9bce9e9f50329a7
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-12-06 14:31:54 +0300

    update cors

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,15 @@
 <?php
 
 return [
-
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
+    | Configure settings for cross-origin resource sharing (CORS).
+    | You can adjust these settings as needed.
     |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    | More info: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
 
@@ -25,9 +23,15 @@ return [
         'https://rgw.zone/'
     ],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Origin',
+        'Content-Type',
+        'X-Auth-Token',
+        'Authorization',
+        'Accept',
+        'X-Requested-With',
+    ],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-
 ];
