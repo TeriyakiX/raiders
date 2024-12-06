d9fe212d7017084b23a2d7ddb63d0d746e73db6c
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-12-06 15:13:09 +0300

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,10 +10,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        if (!$response->headers->has('Access-Control-Allow-Origin')) {
-            $response->headers->set('Access-Control-Allow-Origin', 'https://raiders-front.ru');
-        }
-
+        $response->headers->set('Access-Control-Allow-Origin', 'https://raiders-front.ru');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
